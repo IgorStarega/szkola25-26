a0d22123e8c4749faec4161aa86a742e0738d887
Wilson <staregaigor@gmail.com>
2025-10-08 10:51:20 +0200

    opa

---
+++ desktopy/Na kartkowke/zmienne1.js
@@ -0,0 +1,7 @@
+var liczba1 = Number(prompt("Podaj liczbę 1: "))
+var liczba2 = Number(prompt("Podaj liczbę 2: "))
+var liczba3 = Number(prompt("Podaj liczbę 3: "))
+
+var srednia = (liczba1 + liczba2 + liczba3) / 3
+
+alert("Średnia wynosi: " + srednia)
---
+++ desktopy/Na kartkowke/zmienne2.js
@@ -0,0 +1,6 @@
+var imie = prompt("Podaj imię: ")
+var nazwisko = prompt("Podaj nazwisko: ")
+
+document.write('<a style="color:blue">Imie, </a> ' + '<a>' + imie + '</a>')
+document.write('<br>')
+document.write('<a style="color:red">Nazwisko, </a> ' + '<a>' + nazwisko + '</a>')
