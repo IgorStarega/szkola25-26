88165e33a7d487145e5c205e3f060743839ec878
Wilson <staregaigor@gmail.com>
2025-12-05 13:00:01 +0100

    opa

--- WiAI/Egzamin-restauracja/rezerwacja.php
+++ WiAI/Egzamin-restauracja/rezerwacja.php
@@ -1,13 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rezerwacja</title>
+</head>
+<body>
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data'], $_POST['osoby'], $_POST['telefon'])) {
+    
+    $data_rez = $_POST['data'];
+    $osob = $_POST['osoby'];
+    $nr_tel = $_POST['telefon'];
 
-echo "<script>alert('Dodano rezerwację do bazy;')</script>";
+    $con = mysqli_connect("wilson-db", "root", "rootpassword", "restauracja");
 
-$data_rez=$_POST['data'];
-$osob=$_POST['osoby'];
-$nr_tel=$_POST['telefon'];
+    if (!$con) {
+        die("Błąd połączenia: " . mysqli_connect_error());
+    }
 
-
-$con = mysqli_connect("wilson-db", "root", "rootpassword", "restauracja");
-
-$zapytanie = mysqli_query($con, "INSERT INTO rezerwacje (nr_stolika, data_rez, liczba_osob, telefon) VALUES (5, '$data_rez', $osob, '$nr_tel')");
+    $stmt = mysqli_prepare($con, "INSERT INTO rezerwacje (nr_stolika, data_rez, liczba_osob, telefon) VALUES (5, ?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, "sis", $data_rez, $osob, $nr_tel);
+    
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Dodano rezerwację do bazy";
+    } else {
+        echo "Błąd: " . mysqli_error($con);
+    }
+    
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
+} else {
+    echo "Brak danych z formularza. Wypełnij formularz na stronie głównej.";
+}
 ?>
+</body>
+</html>
