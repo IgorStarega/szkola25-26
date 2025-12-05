3c37423e5bfacb1fc9eb070505aa956c42464b32
Wilson <staregaigor@gmail.com>
2025-12-05 12:45:24 +0100

    opa

--- WiAI/Egzamin-restauracja/rezerwacja.php
+++ WiAI/Egzamin-restauracja/rezerwacja.php
@@ -8,7 +8,12 @@
 <body>
     <?php
     // Połączenie z bazą danych
-    $conn = mysqli_connect("wilson-db", "root", "restauracja", "baza");
+    $conn = mysqli_connect("wilson-db", "root", "", "baza");
+    
+    // Sprawdzenie połączenia
+    if (!$conn) {
+        die("Błąd połączenia: " . mysqli_connect_error());
+    }
 
     // Pobranie danych z formularza
     $data = $_POST['Data'];
@@ -18,10 +23,12 @@
     // Zapytanie INSERT do tabeli rezerwacje (bez nr_stolika)
     $zapytanie = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$osoby', '$telefon')";
     
-    mysqli_query($conn, $zapytanie);
-
-    // Wypisanie komunikatu
-    echo "Dodano rezerwację do bazy";
+    if (mysqli_query($conn, $zapytanie)) {
+        // Wypisanie komunikatu
+        echo "Dodano rezerwację do bazy";
+    } else {
+        echo "Błąd: " . mysqli_error($conn);
+    }
 
     // Zamknięcie połączenia
     mysqli_close($conn);
