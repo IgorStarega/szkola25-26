dd28c29f4a06b1314a2f9af947c01caf1fea126c
Wilson <staregaigor@gmail.com>
2025-12-05 12:35:50 +0100

    opa

---
+++ WiAI/Egzamin-restauracja/rezerwacja.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rezerwacja</title>
+</head>
+<body>
+    <?php
+    // Połączenie z bazą danych
+    $conn = mysqli_connect("localhost", "root", "", "baza");
+
+    // Pobranie danych z formularza
+    $data = $_POST['Data'];
+    $osoby = $_POST['Osoby'];
+    $telefon = $_POST['Tel'];
+
+    // Zapytanie INSERT do tabeli rezerwacje (bez nr_stolika)
+    $zapytanie = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$osoby', '$telefon')";
+    
+    mysqli_query($conn, $zapytanie);
+
+    // Wypisanie komunikatu
+    echo "Dodano rezerwację do bazy";
+
+    // Zamknięcie połączenia
+    mysqli_close($conn);
+    ?>
+</body>
+</html>
