5ed793051e6ddc7e7f63f3f9b3777df9c024d1f9
Wilson <staregaigor@gmail.com>
2025-12-05 12:47:59 +0100

    opa

--- WiAI/Egzamin-restauracja/rezerwacja.php
+++ WiAI/Egzamin-restauracja/rezerwacja.php
@@ -7,31 +7,37 @@
 </head>
 <body>
     <?php
-    // Połączenie z bazą danych
-    $conn = mysqli_connect("wilson-db", "root", "", "baza");
-    
-    // Sprawdzenie połączenia
-    if (!$conn) {
-        die("Błąd połączenia: " . mysqli_connect_error());
-    }
+    // Sprawdzenie, czy formularz został wysłany metodą POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Połączenie z bazą danych
+        $conn = mysqli_connect("wilson-db", "root", "", "baza");
+        
+        // Sprawdzenie połączenia
+        if (!$conn) {
+            die("Błąd połączenia: " . mysqli_connect_error());
+        }
 
-    // Pobranie danych z formularza
-    $data = $_POST['Data'];
-    $osoby = $_POST['Osoby'];
-    $telefon = $_POST['Tel'];
+        // Pobranie danych z formularza
+        $data = $_POST['Data'];
+        $osoby = $_POST['Osoby'];
+        $telefon = $_POST['Tel'];
 
-    // Zapytanie INSERT do tabeli rezerwacje (bez nr_stolika)
-    $zapytanie = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$osoby', '$telefon')";
-    
-    if (mysqli_query($conn, $zapytanie)) {
-        // Wypisanie komunikatu
-        echo "Dodano rezerwację do bazy";
+        // Zapytanie INSERT do tabeli rezerwacje (bez nr_stolika)
+        $zapytanie = "INSERT INTO rezerwacje (data_rez, liczba_osob, telefon) VALUES ('$data', '$osoby', '$telefon')";
+        
+        if (mysqli_query($conn, $zapytanie)) {
+            // Wypisanie komunikatu
+            echo "Dodano rezerwację do bazy";
+        } else {
+            echo "Błąd: " . mysqli_error($conn);
+        }
+
+        // Zamknięcie połączenia
+        mysqli_close($conn);
     } else {
-        echo "Błąd: " . mysqli_error($conn);
+        // Jeśli strona została otwarta bezpośrednio (GET)
+        echo "Brak danych do przetworzenia. Wypełnij formularz na stronie głównej.";
     }
-
-    // Zamknięcie połączenia
-    mysqli_close($conn);
     ?>
 </body>
 </html>
