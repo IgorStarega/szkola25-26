06a0e0e900241bc1dd76b1b35cf07660ce599bfa
Igor Staręga <staregaigor@gmail.com>
2026-04-10 11:51:09 +0200

    opa

--- TSiAI/PHP/Pracownicy-dodawanie-i-edycja/dodaj/debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Current directory: " . __DIR__ . "<br>";
-echo "Real path of database.php: " . realpath(__DIR__ . '/database.php') . "<br>";
-if (file_exists(__DIR__ . '/database.php')) {
-    echo "FILE EXISTS!";
-} else {
-    echo "FILE DOES NOT EXIST!";
-}
